44077b737cb15637487953a7fa0076aa59b7f4fa
Sameer Sonaikar <sonaikar@yahoo.com>
2017-10-26 14:41:53 -0400

    Add a job dsl

---
+++ pipeline.groovy
@@ -0,0 +1,23 @@
+pipeline{
+
+    agent{ node{ label 'master' } }
+    parameters {
+        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+    }
+    triggers { cron('H * * * *')
+    }
+
+    stages{
+        stage('build'){
+            steps{
+                sh 'echo Hello!, "${PERSON}" '
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'I will always say Hello again!'
+        }
+    }
+}
