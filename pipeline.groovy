1c0bda44f6b84bea762fbf63ed0198233ea456ef
Sameer Sonaikar <sonaikar@yahoo.com>
2017-10-27 08:41:56 -0400

    A job dsl

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,23 +1,21 @@
-pipeline{
+pipelineJob('testpipeline') {
+    definition {
+        cps {
+            scm {
+                git {
+                    remote {
+                    // Sets credentials for authentication with the remote repository.
+                    credentials(String d862a434-15e1-4689-9033-e712110f634f)
 
-    agent{ node{ label 'master' } }
-    parameters {
-        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
-    }
-    triggers { cron('H * * * *')
-    }
+                    // Sets a remote URL for a GitHub repository.
+                    github(String ownerAndProject, String protocol = 'https', String host = 'github.com')
 
-    stages{
-        stage('build'){
-            steps{
-                sh 'echo Hello!, "${PERSON}" '
-            }
+                    url(https://github.com/sonaikar/buildscripts.git)
+                    }
+                    branch('master')
+                }
         }
+          scriptPath(Jenkinsfile)
     }
-
-    post {
-        always {
-            echo 'I will always say Hello again!'
-        }
-    }
+}
 }
