803b3c9c080383ce2071766a8dc02bebf58ad580
Sameer Sonaikar <sonaikar@yahoo.com>
2017-10-27 08:44:53 -0400

    A job dsl

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,12 +5,12 @@ pipelineJob('testpipeline') {
                 git {
                     remote {
                     // Sets credentials for authentication with the remote repository.
-                    credentials(String d862a434-15e1-4689-9033-e712110f634f)
+                    credentials(String 'd862a434-15e1-4689-9033-e712110f634f')
 
                     // Sets a remote URL for a GitHub repository.
-                    github(String ownerAndProject, String protocol = 'https', String host = 'github.com')
+                    github(String 'sonaikar', String protocol = 'https', String host = 'github.com')
 
-                    url(https://github.com/sonaikar/buildscripts.git)
+                    url('https://github.com/sonaikar/buildscripts.git')
                     }
                     branch('master')
                 }
